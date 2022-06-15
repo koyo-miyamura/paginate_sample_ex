d4d9ad26634c217dc521ebbc44bb0fe2864da4ec
koyo-miyamura <nexus9071@gmail.com>
2022-06-15 23:41:03 +0900

    CSV変換モジュールのテストを実装

---
+++ test/paginate_sample/utils/csv_converter_test.exs
@@ -0,0 +1,4 @@
+defmodule PaginateSample.Utils.CsvConverterTest do
+  use ExUnit.Case
+  doctest PaginateSample.Utils.CsvConverter
+end
---
+++ test/support/csv_converter/dummy_struct.ex
@@ -0,0 +1,3 @@
+defmodule DummyStruct do
+  defstruct id: nil, name: nil, age: nil
+end
